5d3685e3a9f8063320bad8f6c50ae57dc1cf213b
Hunter Yavorsky <92471345+yavorskyh@users.noreply.github.com>
2023-06-07 15:55:20 -0400

    Added MAUI

---
+++ PP.MAUI/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using PracticePantherMAUI.ViewModels;
+
+namespace PracticePantherMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
